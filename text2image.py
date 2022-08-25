758c03769714642e40e0cba960423dce87bbe84e
Russell Entrikin <russell.entrikin@gmail.com>
2022-08-24 20:27:26 -0400

    better handling of output

--- text2image.py
+++ text2image.py
@@ -1,20 +1,28 @@
 import re
 
+from datetime import datetime
+
 import torch
+
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 
 MODEL_ID = "CompVis/stable-diffusion-v1-4"
 DEVICE   = "cuda"
 
-CHAR_PAT = re.compile(r'[^0-9A-Za-z ]')
+CHAR_PAT      = re.compile(r'[^0-9A-Za-z ]')
+TIMESTAMP_PAT = re.compile(r'[:\.]')
 
 def make_outfilename(prompt):
-    return '_'.join(CHAR_PAT.sub('', prompt).lower().split()) + '.png'
+    title = '-'.join(CHAR_PAT.sub('', prompt).lower().split())
+    now = datetime.now().isoformat(sep='-', timespec='milliseconds')
+    stamp = TIMESTAMP_PAT.sub('-', now)
+    return '_'.join((title, stamp)) + '.png'
 
 if __name__ == '__main__':
 
     import argparse
+    import os
 
     parser = argparse.ArgumentParser(description='Generate image with ' + \
                                                  'Stable Diffusion.')
@@ -23,6 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', '-s', type=int, default=1024)
     parser.add_argument('--guidance_scale', '-g', type=float, default=7.5)
     parser.add_argument('--num_inference_steps', '-n', type=int, default=50)
+    parser.add_argument('--outdir', '-o', type=str, default='output')
     args = parser.parse_args()
 
     pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, 
@@ -36,4 +45,6 @@ if __name__ == '__main__':
                      generator=torch.Generator(DEVICE).manual_seed(args.seed)
                     )["sample"][0]  
     
-    image.save(make_outfilename(args.prompt))
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+    image.save(os.sep.join((args.outdir, make_outfilename(args.prompt))))
